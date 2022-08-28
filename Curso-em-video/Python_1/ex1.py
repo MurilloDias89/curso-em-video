7251726f7ed4a01277fb9f619ab0049c9db2846f
Murilo Cristovao Dias <murilocristovaodias@gmail.com>
2022-08-28 10:29:44 -0300

    v1

---
+++ Curso-em-video/Python_1/ex1.py
@@ -0,0 +1,14 @@
+'''nome=input('Qual é o seu nome? ')
+print('Boas vindas',nome,'!')'''
+
+'''dia=input('Qual dia você nasceu? ')
+mes=input('Qual mês você nasceu? ')
+ano=input('Qual ano você nasceu? ')
+print('Voce nasceu em',dia,'de',mes,'do ano de',ano)'''
+
+n1=int(input('Digite um número: '))
+n2=int(input('Digite um número: '))
+r= n1+n2
+
+print('A soma é de:',r)
+
